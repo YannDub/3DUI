4a3aae0255d619005b4ce3a838509bd2566b1318
Dubois Yann <Dubois Yann>
2017-10-02 11:58:11 +0200

    Add lines in debug mode

---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+	public GameObject targetObject;
+
+	void DrawConnectingLine() {
+		GL.PushMatrix ();
+		GL.LoadIdentity ();
+		GL.Begin (GL.LINES);
+		GL.Color (Color.red);
+		GL.Vertex (this.transform.position);
+		GL.Vertex (targetObject.transform.position);
+		GL.End ();
+		GL.PopMatrix ();
+	}
+
+	public void OnPostRender() {
+		DrawConnectingLine ();
+	}
+
+	void OnDrawGizmos() {
+		Debug.DrawLine (this.transform.position, targetObject.transform.position);
+	}
+}
