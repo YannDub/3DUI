2c2f6d88112f3661b6b134e480e9af6dbbef0ced
Masson Damien <Masson Damien>
2017-11-20 10:14:02 +0100

    Modification code définissant la direction du drone

--- Assets/TrackersSimulation.cs
+++ Assets/TrackersSimulation.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackersSimulation : MonoBehaviour {
-	private GameObject tracker;
+public class TrackersSimulation : MonoBehaviour
+{
+    private GameObject tracker;
 
-	private Plane objectPlane;
-	private GameObject selectedObject = null;
+    private Plane objectPlane;
+    private GameObject selectedObject = null;
     private GameObject myLine;
 
     public Quadcopter drone;
@@ -15,10 +16,11 @@ public class TrackersSimulation : MonoBehaviour {
 
     private LineRenderer lr;
 
-	public string side;
+    public string side;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         tracker = this.gameObject;
         myLine = new GameObject();
 
@@ -28,83 +30,121 @@ public class TrackersSimulation : MonoBehaviour {
         lr.endWidth = lr.startWidth = 0.02f;
     }
 
-	void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f) {
+    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
+    {
         myLine.transform.position = start;
         lr.startColor = lr.endColor = color;
         lr.SetPosition(0, start);
-		lr.SetPosition(1, end);
-	}
+        lr.SetPosition(1, end);
+    }
 
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			tracker.transform.Rotate (new Vector3 (0, -1, 0));
-		} 
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			tracker.transform.Rotate (new Vector3 (0, 1, 0));
-		} 
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			tracker.transform.Rotate (new Vector3 (-1, 0, 0));
-		} 
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			tracker.transform.Rotate (new Vector3 (1, 0, 0));
-		}
 
-		if (Input.GetMouseButton (0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-			float rayDistance;
-			if (selectedObject != null) {
-				Vector3 mousePos = Input.mousePosition;
-				mousePos.y = Screen.height - mousePos.y;
-				if (objectPlane.Raycast (ray, out rayDistance)) {
-					selectedObject.transform.position = ray.GetPoint (rayDistance);
-				}
-			} else if (Physics.Raycast (ray, out hit) && hit.collider.gameObject == tracker) {
-				selectedObject = hit.collider.gameObject;
-				objectPlane = new Plane (Camera.main.transform.forward, hit.collider.transform.position);
-			} else {
-				selectedObject = null;
-			}
-		}
-
-		if (Input.GetKeyDown (KeyCode.R)) {
-			tracker.transform.rotation = new Quaternion ();
-		}
-
-		Ray r = new Ray (tracker.transform.position, tracker.transform.rotation * new Vector3 (0, 0, 1));
-
-		/*if (Input.GetKeyDown (KeyCode.E)) {
-			movement.startLerping(tracker.transform.forward, tracker.transform.position);
-		}*/
-
-		DrawLine (tracker.transform.position, r.GetPoint (100), Color.red);
-
+    Vector2 GetYawPitch(Vector3 dir)
+    {
         Vector3 direction = GetDirection();
 
-        Vector3 dronePos = drone.transform.up;
+        Vector3 dronePos = dir;
 
         float pitch = Mathf.Asin(direction.y);
         float pitchb = Mathf.Asin(dronePos.y);
 
         float yaw = Mathf.Asin(direction.x / Mathf.Cos(pitch)); //Beware cos(pitch)==0, catch this exception!
         float yawb = Mathf.Asin(dronePos.x / Mathf.Cos(pitchb));
-        float roll = 0;
 
         float truePitch = pitchb - pitch;
         float trueYaw = yawb - yaw;
-        float power = 0;//Input.GetAxisRaw("Power");
-        if (Input.GetKey(KeyCode.Space))
+
+        return new Vector2(trueYaw, truePitch);
+    }
+
+
+
+
+    bool lockPitch = false;
+    bool lockYaw = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            power = 1.0f;
-
-            Debug.Log(power + " " + truePitch + " " + trueYaw + " " + roll);
-
-            // Flys the quadcopter using the inputs
-            drone.Drive(power, truePitch, trueYaw, roll);
+            tracker.transform.Rotate(new Vector3(0, -1, 0));
         }
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tracker.transform.Rotate(new Vector3(0, 1, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tracker.transform.Rotate(new Vector3(-1, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tracker.transform.Rotate(new Vector3(1, 0, 0));
+        }
 
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            float rayDistance;
+            if (selectedObject != null)
+            {
+                Vector3 mousePos = Input.mousePosition;
+                mousePos.y = Screen.height - mousePos.y;
+                if (objectPlane.Raycast(ray, out rayDistance))
+                {
+                    selectedObject.transform.position = ray.GetPoint(rayDistance);
+                }
+            } else if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == tracker)
+            {
+                selectedObject = hit.collider.gameObject;
+                objectPlane = new Plane(Camera.main.transform.forward, hit.collider.transform.position);
+            } else
+            {
+                selectedObject = null;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            tracker.transform.rotation = new Quaternion();
+        }
+
+        Ray r = new Ray(tracker.transform.position, tracker.transform.rotation * new Vector3(0, 0, 1));
+
+        /*if (Input.GetKeyDown (KeyCode.E)) {
+			movement.startLerping(tracker.transform.forward, tracker.transform.position);
+		}*/
+
+        DrawLine(tracker.transform.position, r.GetPoint(100), Color.red);
+
+
+
+        float power = Input.GetAxisRaw("Power");
+
+        //Debug.Log(power + " " + truePitch + " " + trueYaw + " " + roll);
+
+        // Flys the quadcopter using the inputs
+
+        //if (Mathf.Abs(trueYaw) <= 0.1) lockYaw = true;
+        //if (Mathf.Abs(truePitch) <= 0.1) lockPitch = true;
+
+        float trueYaw = GetYawPitch(drone.transform.forward).x;
+        float truePitch = GetYawPitch(drone.transform.up).y;
+
+        if (lockYaw) trueYaw = 0;
+        if (lockPitch) truePitch = 0;
+
+        if (Mathf.Abs(truePitch) <= 0.1 && Mathf.Abs(trueYaw) <= 0.1)
+        {
+            lockYaw = lockPitch = true;
+            Debug.Log("Fin rotation");
+            drone.Drive(0, 0, 0, 0);
+        }
+
+        drone.Drive(0.1f, truePitch, trueYaw, 0);
+
+        Debug.Log(drone.transform.up + " " + tracker.transform.forward);
 
 
         /*if (side == "left") {
@@ -115,7 +155,8 @@ public class TrackersSimulation : MonoBehaviour {
 
     }
 
-    public Vector3 GetDirection() {
+    public Vector3 GetDirection()
+    {
         return Vector3.Normalize(tracker.transform.rotation * new Vector3(0, 0, 1));
     }
 }
